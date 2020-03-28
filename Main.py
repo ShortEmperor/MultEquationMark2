c74a4211f706496e2da30d4480df16108a2cbafc
Fabricio Escalante <ShortEmperor194@gmail.com>
2020-03-28 15:51:23 -0600

    Translate everything to english

--- Main.py
+++ Main.py
@@ -1,57 +1,59 @@
 #!/usr/bin/env python
+#This code is only intended for linux
+
+
 
 import os
 
 def main():
 	try:
-		#Comment the following lines if not in windows
 		os.system("clear")
-		print("Definicion de la Matriz")
+		print("Defining the matrix")
 
 
-		print("Ingrese los valores: ")
+		print("Type in the values: ")
 		print("|x1 y1 z1 r1|")
 		print("|x2 y2 z2 r2|")
 		print("|x3 y3 z3 r3|")
 
-		x1 = float(input('Ingrese el valor x1: \n>>'))
-		y1 = float(input('Ingrese el valor y1: \n>>'))
-		z1 = float(input('Ingrese el valor z1: \n>>'))
-		r1 = float(input('Ingrese el valor r1: \n>>'))
-		x2 = float(input('Ingrese el valor x2: \n>>'))
-		y2 = float(input('Ingrese el valor y2: \n>>'))
-		z2 = float(input('Ingrese el valor z2: \n>>'))
-		r2 = float(input('Ingrese el valor r2: \n>>'))
-		x3 = float(input('Ingrese el valor x3: \n>>'))
-		y3 = float(input('Ingrese el valor y3: \n>>'))
-		z3 = float(input('Ingrese el valor z3: \n>>'))
-		r3 = float(input('Ingrese el valor r3: \n>>'))
+		x1 = float(input('Type the value for x1: \n>>'))
+		y1 = float(input('Type the value for y1: \n>>'))
+		z1 = float(input('Type the value for z1: \n>>'))
+		r1 = float(input('Type the value for r1: \n>>'))
+		x2 = float(input('Type the value for x2: \n>>'))
+		y2 = float(input('Type the value for y2: \n>>'))
+		z2 = float(input('Type the value for z2: \n>>'))
+		r2 = float(input('Type the value for r2: \n>>'))
+		x3 = float(input('Type the value for x3: \n>>'))
+		y3 = float(input('Type the value for y3: \n>>'))
+		z3 = float(input('Type the value for z3: \n>>'))
+		r3 = float(input('Type the value for r3: \n>>'))
 
 
-		#Determinante del Sistema
+		#System determiner
 
 		DetSys = ((x1*y2*z3)+(x2*y3*z1)+(x3*y1*z2))-((x3*y2*z1)+(z2*y3*x1)+(z3*y1*x2))
 
-		#Determinante de X
+		#X determiner
 		DetX = ((r1*y2*z3)+(r2*y3*z1)+(r3*y1*z2))-((r3*y2*z1)+(z2*y3*r1)+(z3*y1*r2))
 
-		#Determinante de Y
+		#Y determiner
 		DetY = ((x1*r2*z3)+(x2*r3*z1)+(x3*r1*z2))-((x3*r2*z1)+(z2*r3*x1)+(z3*r1*x2))
 
-		#Determinante de Z
+		#Z determiner
 		DetZ = ((x1*y2*r3)+(x2*y3*r1)+(x3*y1*r2))-((x3*y2*r1)+(r2*y3*x1)+(r3*y1*x2))
-		
-		#Obtencion de las incognitas
+
+		#Obtaining the results
 
 		X= DetX/DetSys
 		Y= DetY/DetSys
 		Z= DetZ/DetSys
 
-		print("Los resultados son: ", "\nX=",X,"\nY=",Y,"\nZ=",Z)
+		print("The results are: ", "\nX=",X,"\nY=",Y,"\nZ=",Z)
 
-		#Repeticion de la funcion
-		rep = str(input("Quieres repetir la operacion?(S/n): \n>>"))
-		if rep.upper() == "S":
+		#Repeating the program
+		rep = str(input("Do you wanna repeat the program? (Y/n): \n>>"))
+		if rep.upper() == "Y":
 			main()
 		elif rep.upper() == "N":
 			os.system("clear")
